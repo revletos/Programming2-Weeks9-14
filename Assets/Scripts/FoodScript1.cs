e5425da38bb88dbe58aa2ea602ccda0d82fbd582
Jay <belin@OA-S143-06>
2025-04-15 21:04:41 -0400

    Dirty/Spaghetti Code for coroutine and starting to clean up and comment out code

--- Assets/Scripts/FoodScript1.cs
+++ Assets/Scripts/FoodScript1.cs
@@ -14,6 +14,11 @@ public class FoodScript1 : MonoBehaviour
     public GameObject teaprefabtext;
     public GameObject watermelonindicator1;
     public GameObject watermelonindicator2;
+    public GameObject cupcakeindicator1;
+    public GameObject cupcakeindicator2;
+    public GameObject cupcakeindicator3;
+    public GameObject teaindicator1;
+    public GameObject teaindicator2;
     public SpriteRenderer watermelon;
     public SpriteRenderer cupcake;
     public SpriteRenderer tea;
@@ -23,6 +28,8 @@ public class FoodScript1 : MonoBehaviour
     public float t;
 
     Coroutine watermelonindicator;
+    Coroutine cupcakeindicator;
+    Coroutine teaindicator;
     //public Color colourChange;
     bool mouseOver;
     float hue;
@@ -33,6 +40,12 @@ public class FoodScript1 : MonoBehaviour
         t = 0;
         watermelonindicator1.SetActive(false);
         watermelonindicator2.SetActive(false);
+        cupcakeindicator1.SetActive(false);
+        cupcakeindicator2.SetActive(false);
+        cupcakeindicator3.SetActive(false);
+        teaindicator1.SetActive(false);
+        teaindicator2.SetActive(false);
+
     }
 
     // Update is called once per frame
@@ -47,9 +60,11 @@ public class FoodScript1 : MonoBehaviour
 
         if (watermelonindicator != null)
         {
-            if (t <= 5)
+            if (t <= 2) // if the value of t is less than 2
             {
-                t = Time.deltaTime*300;
+                t = Time.deltaTime * 300; // increase t
+                                          // yes, I know this looks atrocious. Page 6 of my implementation log explains the madness somewhat
+                                          // I'm very out of it and the dirty fix did relatively what I needed it to. 
             }
             else
             {
@@ -58,76 +73,89 @@ public class FoodScript1 : MonoBehaviour
                 watermelonindicator2.SetActive(false);
                 t = 0;
             }
-            
+
         }
 
-
-
-        if (watermelon.bounds.Contains(mouse) && Input.GetMouseButtonDown(0))
+        if (cupcakeindicator != null) // same code just for the cupcake
+                                      // don't see much logic is recommenting it
         {
-            Debug.Log("a");
-            //slime.transform.localScale = new Vector3(slime.transform.localScale.x*1.1f, slime.transform.localScale.y*1.1f, 2);
-            //slime.transform.localScale = new Vector3(slime.transform.localScale.x * 0.9f, slime.transform.localScale.y * 0.9f, 2);
-            //colourChange = new Color(0, 250, 0);
-            //slime.color = colourChange;
-            //hue = Random.Range(0f, 1f);
-            //slime.color = Color.HSVToRGB(hue, 1, 1);
+            if (t <= 2)
+            {
+                t = Time.deltaTime * 300;
+            }
+            else
+            {
+                StopCoroutine(cupcakeindicator);
+                cupcakeindicator1.SetActive(false);
+                cupcakeindicator2.SetActive(false);
+                cupcakeindicator3.SetActive(false);
+                t = 0;
+            }
+
         }
 
+        if (teaindicator != null)     // same code just for the tea
+                                      // don't see much logic is recommenting it
+        {
+            if (t <= 2)
+            {
+                t = Time.deltaTime * 300;
+            }
+            else
+            {
+                StopCoroutine(teaindicator);
+                teaindicator1.SetActive(false);
+                teaindicator2.SetActive(false);
+                t = 0;
+            }
 
-        //    if (sr.bounds.Contains(mouse))
-        //    {
-        //        //if (lastTime == false)
-        //        //{
-        //            OverTarget();
-        //            Debug.Log("over");
+        }
 
-        //            //GameObject newText = Instantiate(prefabtext, mouse, Quaternion.identity);
-        //        //}
-        //    }
-        //    else
-        //    {
-
-        //        ;//lastTime = false;
-        //    }
-        //    //lastTime = sr.bounds.Contains(mouse);
-
-        //}
-
-        //public void OverTarget()
-        //{
-        //    //GameObject newText = Instantiate(prefabtext, mouse, Quaternion.identity);
-        //    prefabtext.SetActive(true);
 
     }
 
-    public void Watermelon()
+    public void Watermelon()  //Watermelon event
     {
         slime.transform.localScale = new Vector3(slime.transform.localScale.x * 1.1f, slime.transform.localScale.y * 1.1f, 2); // Increase the size of the slime by 110% its current size in both x and y scales
-        watermelonindicator = StartCoroutine(SpawnIndicator());
-        Debug.Log(t);
+        watermelonindicator = StartCoroutine(SpawnIndicatorWatermelon()); // Start the watermelon indicator coroutine (line 61)
 
     }
 
     public void Cupcake()
     {
         slime.transform.localScale = new Vector3(slime.transform.localScale.x * 0.9f, slime.transform.localScale.y * 0.9f, 2);
+        cupcakeindicator = StartCoroutine(SpawnIndicatorCupcake()); // Start the cupcake indicator coroutine (line 61)
     }
 
     public void Drink()
     {
         hue = Random.Range(0f, 1f);
         slime.color = Color.HSVToRGB(hue, 1, 1);
+        teaindicator = StartCoroutine(SpawnIndicatorTea());
     }
 
-    IEnumerator SpawnIndicator()
+    IEnumerator SpawnIndicatorWatermelon()
     {
         watermelonindicator1.SetActive(true);
         watermelonindicator2.SetActive(true);
         yield return null;
 
+    }
 
+    IEnumerator SpawnIndicatorCupcake()
+    {
+        cupcakeindicator1.SetActive(true);
+        cupcakeindicator2.SetActive(true);
+        cupcakeindicator3.SetActive(true);
+        yield return null;
 
+    }
+
+    IEnumerator SpawnIndicatorTea()
+    {
+        teaindicator1.SetActive(true);
+        teaindicator2.SetActive(true);
+        yield return null;
     }
 }
 

4129b767ff2a231d2cd20b17a05ae828f330a1b6
Jay <belin@OA-S143-06>
2025-04-15 21:12:49 -0400

    Commented Slime Movement Script

--- Assets/Scripts/Slimemovement.cs
+++ Assets/Scripts/Slimemovement.cs
@@ -17,17 +17,17 @@ public class Slimemovement : MonoBehaviour
     {
         
     }
-     public void SlimeMoving()
+     public void SlimeMoving() // Simple movement from Week 1, written as a method so it could be used as a event in another script 
     {
-        Vector2 pos = transform.position;
-        pos.x += speed * Time.deltaTime;
-        Vector2 slimemove = Camera.main.WorldToScreenPoint(pos);
+        Vector2 pos = transform.position; // declare pos as a Vector 2
+        pos.x += speed * Time.deltaTime; // Position is increased by speed * Delta Time
+        Vector2 slimemove = Camera.main.WorldToScreenPoint(pos); // declare slimemove as a Vector 2 in screenspace
 
-        if (slimemove.x < 0 + 100 || slimemove.x > Screen.width - 100)
+        if (slimemove.x < 0 + 100 || slimemove.x > Screen.width - 100) // If the slime reaches the predetermined border, have it go in the opposite direction
         {
-            speed = speed * -1;
+            speed = speed * -1; // Positive/Negative value determines change on x axis
         }
         transform.position = pos;
-        //Debug.Log(slimemove.x);
+        //Debug.Log(slimemove.x); // Used for determining movement constraints
     }
 }
